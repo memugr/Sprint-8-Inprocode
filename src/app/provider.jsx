55c5fc42188febd9332e089b6805807a892ea7fc
mei <mmullengracia@gmail.com>
2025-02-10 11:09:31 +0100

    Set up initial states in the App provider

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
 import { AppContext } from "../context/AppContext";
 
 export function AppProvider({ children }) {
+    const [state, setState] = useState({
+        user: null,
+        isAuth: false,
+        username: '',
+        email: '',
+        isLoading: true,
+    })
+
+    const value = {
+        state,
+        setState,
+        login: (user) => {
+            setState((prevState) => ({
+                ...prevState,
+                user,
+                isAuth: true,
+            }));
+        },
+        logout: () => {
+            setState((prevState) => ({
+                ...prevState,
+                user: null,
+                isAuth: false,
+            }));
+        },
+        setUsername: (username) => {
+            setState((prevState) => ({
+                ...prevState,
+                username,
+            }));
+        },
+        setEmail: (email) => {
+            setState((prevState) => ({
+                ...prevState,
+                email,
+            }));
+        },
+        setLoading: (isLoading) => {
+            setState((prevState) => ({
+                ...prevState,
+                isLoading,
+            }));
+        },
+    };
+
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={value}>
             {children}
         </AppContext.Provider>
     )
